1fdff5858836531b4c6d55ce5d1f7c32e20c4ad5
Neoklis Varsamos <neoklis.varsamos@peoplecert.org>
2023-07-06 10:25:08 +0300

    minor changes

--- RPC-Server/RPC-Server/Calculator.cs
+++ RPC-Server/RPC-Server/Calculator.cs
@@ -11,23 +11,30 @@ namespace RPC_Server
     {
         internal static float? Calculate(MessageModel model)
         {
-            if (model.Task == TaskType.Addition)
+            try
             {
-                return model.Number1 + model.Number2;
+                if (model.Task == TaskType.Addition)
+                {
+                    return model.Number1 + model.Number2;
+                }
+                else if (model.Task == TaskType.Subtraction)
+                {
+                    return model.Number1 - model.Number2;
+                }
+                else if (model.Task == TaskType.Multiplication)
+                {
+                    return model.Number1 * model.Number2;
+                }
+                else if (model.Task == TaskType.Division)
+                {
+                    return model.Number1 / model.Number2;
+                }
+                else
+                {
+                    return null;
+                }
             }
-            else if (model.Task == TaskType.Subtraction)
-            {
-                return model.Number1 - model.Number2;
-            }
-            else if (model.Task == TaskType.Multiplication)
-            {
-                return model.Number1 * model.Number2;
-            }
-            else if (model.Task == TaskType.Division)
-            {
-                return model.Number1 / model.Number2;
-            }
-            else
+            catch (Exception)
             {
                 return null;
             }
